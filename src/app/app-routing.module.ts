89fbd98465d27a3c9ee25dbedcadd2dcaf8b4893
Oleksii Lysenko <nano77@ukr.net>
2021-04-06 22:13:10 +0300

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponent } from './first/first.component';
+import { ItemComponent } from './item/item.component';
+
+const routes: Routes = [
+  { path: '', component: FirstComponent },
+  { path: ':id', component: ItemComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FirstComponent } from './first/first.component';
+import { ItemComponent } from './item/item.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FirstComponent,
+    ItemComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
